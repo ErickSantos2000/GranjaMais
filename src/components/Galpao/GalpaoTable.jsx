ed8e655a03e211a4465d345cd65f8d6c69abbad8
ErickSantos2000 <devErickFelipe@gmail.com>
2026-02-26 01:31:26 -0300

    adicionando logica do GalpoesTable.jsx(listagem)

--- src/components/Galpao/GalpaoTable.jsx
+++ src/components/Galpao/GalpaoTable.jsx
@@ -1,49 +1,50 @@
 
 
-// import Table from 'react-bootstrap/Table';
+import Table from 'react-bootstrap/Table';
 
-// function GalpaoTable({ galpoes }) {
-//   if(!galpoes || galpoes.length === 0){
-//     return <p>Nenhum galpão encontrado.</p>;
-//   }
+function GalpaoTable({ galpoes }) {
+  if(!galpoes || galpoes.length === 0){
+    return <p>Nenhum galpão encontrado.</p>;
+  }
 
-//   return (
-//     <Table >
-//       <thead>
-//         <tr>
-//           <th>Nome</th>
-//           <th>Frangos</th>
-//           <th>Comedouros</th>
-//           <th>Bebedouros</th>
-//           <th>Ventiladores</th>
-//           <th>Exaustores</th>
-//           <th>Responsáveis</th>
-//           <th>Termômetro</th>
-//         </tr>
-//       </thead>
+  return (
+    <Table striped bordered hover responsive>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Nome</th>
+          <th>Frangos</th>
+          <th>Comedouros</th>
+          <th>Bebedouros</th>
+          <th>Ventiladores</th>
+          <th>Exaustores</th>
+          <th>Responsáveis</th>
+          <th>Termômetro</th>
+        </tr>
+      </thead>
 
-//       <tbody>
-//         {
-//           galpoes.map((galpao) => (
-//             <tr key={galpao.id}>
-//               <td>{galpao.id}</td>
-//               <td>{galpao.nome}</td>
-//               <td>{galpao.qtdFrangos}</td>
-//               <td>{galpao.qtdComedouros}</td>
-//               <td>{galpao.qtdBebedouros}</td>
-//               <td>{galpao.qtdVentiladores}</td>
-//               <td>{galpao.qtdExaustores}</td>
-//               <td>{galpao.qtdResponsaveis}</td>
-//               <td>{galpao.temTermometro ? "Ativo" : "Inativo"}</td>
-//           </tr>
-//           )
-//         )          
-//       }
-//       </tbody>
+      <tbody>
+        {
+          galpoes.map((galpao) => (
+            <tr key={galpao.id}>
+              <td>{galpao.id}</td>
+              <td>{galpao.nome}</td>
+              <td>{galpao.qtdFrangos}</td>
+              <td>{galpao.qtdComedouros}</td>
+              <td>{galpao.qtdBebedouros}</td>
+              <td>{galpao.qtdVentiladores}</td>
+              <td>{galpao.qtdExaustores}</td>
+              <td>{galpao.qtdResponsaveis}</td>
+              <td>{galpao.temTermometro ? "Ativo" : "Inativo"}</td>
+          </tr>
+          )
+        )          
+      }
+      </tbody>
       
-//     </Table>
-//   );
-// }
+    </Table>
+  );
+}
 
-// export default GalpaoTable;
+export default GalpaoTable;
 
--- src/pages/Galpoes.jsx
+++ src/pages/Galpoes.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'; // importa os hoocks
+import galpaoService from '../services/galpaoService'; // importa o serviço
 import { Container } from 'react-bootstrap';
 import GalpaoTable from '../components/Galpao/GalpaoTable';
 
 export default function Galpoes() {
+    // criando estado para guardar listas de galpoes
+    const[galpoes, setGalpoes] = useState([]);
+
+    // executa assim que uma função a pagina é montada na tela
+    useEffect(() => {
+      const buscarDados = async () => {
+        try {
+          const dados = await galpaoService.listar(); // chama o get do serviço
+          setGalpoes(dados); // salva os dados recebidos no estado
+        } catch (error) {
+          console.error("Erro ao buscar galpões:", error);
+        }
+      }
+      buscarDados();
+
+    }, []);
   return (
+
     <Container className="mt-5 pt-5"> {/* Adicionado mt-5 pt-5 */}
       
-      {/* <GalpaoTable></GalpaoTable> */}
+      <GalpaoTable galpoes={galpoes} />
+
     </Container>
   );
 }
